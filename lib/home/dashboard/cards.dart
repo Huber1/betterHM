17d94b2612287455c6f028a55479beee4c701f33
Moritz <2nd@moritzhuber.de>
2023-06-27 12:26:41 +0200

    remove testcard

--- lib/home/dashboard/cards.dart
+++ lib/home/dashboard/cards.dart
@@ -6,8 +6,6 @@ import 'package:better_hm/home/dashboard/semester_status/api_semester_status.dar
 import 'package:better_hm/home/dashboard/semester_status/models/semester_event.dart';
 import 'package:better_hm/home/dashboard/semester_status/semester_status.dart';
 import 'package:better_hm/i18n/strings.g.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 
 final cards = <DashboardCard>{
   DashboardCard<List<SemesterEvent>>(
@@ -23,12 +21,12 @@ final cards = <DashboardCard>{
     future: () =>
         ApiMvg().getDepartures(stopId: stopIdLothstr, lineIds: lineIdsLothstr),
   ),
-  DashboardCard(
-    title: "TESTCARD",
-    cardId: "TESTCARD",
-    card: (_) => SvgPicture.network(
-      "https://www.mvv-muenchen.de/fileadmin/lines/02021.svg",
-    ),
-    future: () => Future.value(),
-  ),
+  // DashboardCard(
+  //   title: "TESTCARD",
+  //   cardId: "TESTCARD",
+  //   card: (_) => SvgPicture.network(
+  //     "https://www.mvv-muenchen.de/fileadmin/lines/02021.svg",
+  //   ),
+  //   future: () => Future.value(),
+  // ),
 };
